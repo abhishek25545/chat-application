d338767fddfef268e718dcb2bda57b83429b16d2
abhishek a <113160661+abhishek25545@users.noreply.github.com>
2022-11-14 23:57:22 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('<str:room>/', views.room, name='room'),
-    path('checkview', views.checkview, name='checkview'),
-    path('send', views.send, name='send'),
-    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+    path('admin/', admin.site.urls),
+    path('', include('chat.urls'))
 ]
